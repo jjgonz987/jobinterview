fef1caba05dd34014f5779b084a79602c540a423
jjgonz987 <jhovany990@gmail.com>
2020-03-31 02:19:43 -0500

    rm readme boilerplate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>fuck off</h1>
+        <h1>hello world</h1>
       </header>
     </div>
   );
